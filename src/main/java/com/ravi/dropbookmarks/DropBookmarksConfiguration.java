3afc9c84d2415305b66f1895bb908d5ced0944ff
saiganesh.popuri <sganesh@blackberry.com>
2023-01-08 21:00:55 +0530

    dropwizard

---
+++ src/main/java/com/ravi/dropbookmarks/DropBookmarksConfiguration.java
@@ -0,0 +1,10 @@
+package com.ravi.dropbookmarks;
+
+import io.dropwizard.core.Configuration;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.validator.constraints.*;
+import jakarta.validation.constraints.*;
+
+public class DropBookmarksConfiguration extends Configuration {
+    // TODO: implement service configuration
+}
